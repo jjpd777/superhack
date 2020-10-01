b7c2e277b3052947594ec4e6dc5e4fc25ae030b7
juan <jpalacio@axletravel.com>
2020-10-01 14:45:11 -0600

    Routing implemented

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,21 @@
+import app from 'firebase/app';
+
+  var config = {
+apiKey: "AIzaSyDXCQrmUjq-Whya-_1ImzEYv4UHYkaRLkY",
+authDomain: "suuperhack-777.firebaseapp.com",
+databaseURL: "https://suuperhack-777.firebaseio.com",
+projectId: "suuperhack-777",
+storageBucket: "suuperhack-777.appspot.com",
+messagingSenderId: "982482990578",
+appId: "1:982482990578:web:923b5bcb442c6c20a3676b",
+measurementId: "G-Z2QG9LYTWL"
+};
+
+class Firebase {
+    constructor() {
+      app.initializeApp(config);
+    }
+  }
+   
+  export default Firebase;
+
